18b88e2379c0abc5d659daa0ab7e7207a18467ca
Sahil Kanungo <kanungosahil123@gmail.com>
2023-03-05 23:42:02 +0530

    created Register Activity

--- app/src/main/java/com/example/androidjavatutorial/LoginActivity.java
+++ app/src/main/java/com/example/androidjavatutorial/LoginActivity.java
@@ -2,13 +2,45 @@ package com.example.androidjavatutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editUserName , editUserPassword;
+    Button loginBtn;
+    TextView signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editUserName = findViewById(R.id.editTextLogInUserName);
+        editUserPassword = findViewById(R.id.editTextLogInPassword);
+        loginBtn = findViewById(R.id.buttonLogin);
+        signIn = findViewById(R.id.textViewNewUser);
+
+        loginBtn.setOnClickListener(view -> {
+            String userName = editUserName.getText().toString();
+            String password = editUserPassword.getText().toString();
+
+            if(userName.isEmpty() || password.isEmpty()){
+                Toast.makeText(getApplicationContext(),"Please Fill all details",Toast.LENGTH_SHORT)
+                        .show();
+            }else {
+                Toast.makeText(getApplicationContext(), "Login Successfully", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        signIn.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidjavatutorial/RegisterActivity.java
@@ -0,0 +1,61 @@
+package com.example.androidjavatutorial;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUserName , edUserEmail, edUserPassword , edConfirmPassword;
+    Button registerBtn;
+    TextView tv;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        edUserName = findViewById(R.id.editTextSignInUserName);
+        edUserEmail = findViewById(R.id.editTextFullName);
+        edUserPassword = findViewById(R.id.editTextPassword);
+        edConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+        registerBtn = findViewById(R.id.buttonRegister);
+
+        tv = findViewById(R.id.textViewAlreadyUser);
+
+        registerBtn.setOnClickListener(view -> {
+            String userName = edUserName.getText().toString();
+            String userEmail = edUserEmail.getText().toString();
+            String userPassword = edUserPassword.getText().toString();
+            String userConfirmPassword = edConfirmPassword.getText().toString();
+
+            if(!userName.isEmpty() && !userEmail.isEmpty() &&
+                    !userPassword.isEmpty() && !userConfirmPassword.isEmpty()) {
+                if(userPassword.equals(userConfirmPassword)){
+                    Toast.makeText(getApplicationContext(),"Successfully Register",
+                            Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                } else{
+                    Toast.makeText(getApplicationContext(),
+                            "Password does not match  Try Again !!",Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Fill all the details",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv.setOnClickListener(view -> startActivity(new Intent(RegisterActivity.this,LoginActivity.class)));
+
+
+    }
+}
